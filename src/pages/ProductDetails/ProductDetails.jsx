3793a94d70be24139971455a169af792a675e20a
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-04-27 22:33:28 +0600

    _redirects  added

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -3,7 +3,7 @@ import useProducts from "../../hooks/useProducts";
 import { useEffect, useState } from "react";
 
 const ProductDetails = () => {
-    const [ products ] = useProducts();
+    const [products] = useProducts();
     const [product, setProduct] = useState({});
     const { unique_id, name, short_desc, image, stock, price, buying_price, discount_amount, discount_date, updated_at, category } = product;
 
@@ -11,13 +11,13 @@ const ProductDetails = () => {
     // console.log('Product id:', id);
 
     useEffect(() => {
-        const fetchProduct = async() => {
-            try{
+        const fetchProduct = async () => {
+            try {
                 const res = await products.find(prd => prd?.unique_id === id);
-                if(res){
+                if (res) {
                     setProduct(res || {});
                 }
-            }catch(err){
+            } catch (err) {
                 console.error(err);
                 return {};
             }
@@ -25,7 +25,7 @@ const ProductDetails = () => {
         fetchProduct();
     }, [products, id]);
 
-    // console.log(product)
+    console.log(product)
 
     return (
         <section className="product-deatails">
@@ -36,12 +36,27 @@ const ProductDetails = () => {
                     <p className="text-xs bg-green-50 inline-block border border-green-300 rounded-full px-3 py-[2px]">{category?.name}</p>
                     <div className="flex items-center justify-between my-1.5">
                         <h1 className="text-lg md:text-xl text-slate-700 font-medium">{name}</h1>
-                        <p className="text-lg md:text-xl font-medium text-orange-600">$<span>{price}</span></p>
+                        <div className="flex items-center">
+                            <p className="text-lg md:text-xl font-medium text-orange-600 me-2">$<span>{price}</span></p>
+                            <h1 className="text-xs px-2 rounded-sm bg-orange-100 text-orange-700">
+                                -<span className="ps-[2px]">$</span>
+                                <span>{discount_amount ? discount_amount : 0}</span>
+                            </h1>
+                        </div>
+                    </div>
+
+                    <div className="my-3">
+                        <h1 className="text-green-700 font-medium">In stock: ({stock ? stock : 0})</h1>
+                        <p className="text-sm text-gray-500">Last update: {updated_at ? updated_at.split('T')[0] : 'Not available'}</p>
+                    </div>
+
+                    <div>
+                        <p>{short_desc}</p>
                     </div>
                 </div>
             </div>
         </section>
-    ); 
+    );
 };
 
 export default ProductDetails;
